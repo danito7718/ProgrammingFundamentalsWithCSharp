f89e1581d3f914fe49312a87b6bfcbcb1dfd8897
danito7718 <danito7718@gmail.com>
2025-02-09 22:09:31 +0200

    Добавен Word файл с описание на задачите

--- 09ExercsieDataTypesAndVariables/09ExercsieDataTypesAndVariables/02SumDigits/obj/Debug/net8.0/02SumDigits.AssemblyInfo.cs
+++ 09ExercsieDataTypesAndVariables/09ExercsieDataTypesAndVariables/02SumDigits/obj/Debug/net8.0/02SumDigits.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02SumDigits")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6216bacfe15010f5f876d8c7c345c5dacfba8d80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63b7771c2f8442bf8ae2cd802d05e6cb35884e3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("02SumDigits")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02SumDigits")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
